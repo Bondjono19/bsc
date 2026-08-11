12517a514c35573a49999e0d9d5ad22b2b60e3bd
Marius <marius@sinkbaek.dk>
2026-08-11 19:29:38 +0200

    monitor change

--- recognition/utils/monitor.sh
+++ recognition/utils/monitor.sh
@@ -4,7 +4,7 @@ while true; do
   mem=$(free -m | awk '/^Mem:/ {print $7}')     
   swap=$(free -m | awk '/^Swap:/ {print $3}')    
   temp=$(vcgencmd measure_temp | grep -o '[0-9.]*')
-  throttled=$(vcgencmd get_throttled)
+  throttled=$(vcgencmd get_throttled | cut -d= -f2)
   echo "$ts,$mem,$swap,$temp,$throttled" >> sysmon.csv
   sleep 1
 done
